de5c248745c2061154d710c5cb3cb416f4255b65
Shivam Pandey <shivam.pandey@makunaiglobal.ai>
2026-04-06 15:17:30 +0530

    removd code

--- src/components/common/Card/index.ts
+++
@@ -1,2 +0,0 @@
-export { default, CardHeader, CardBody, CardFooter, CardTitle, CardSubtitle, CardDescription } from './Card';
-export type { CardProps, CardHeaderProps, CardBodyProps, CardFooterProps, CardTitleProps, CardSubtitleProps, CardDescriptionProps } from './Card';
